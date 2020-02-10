7b74871152c73bc65edfe1061bc0d0fc64291721
just try it <614841384@qq.com>
2020-02-10 10:12:30 +0800

    Create test02.go
    Go应用程序的入口必须是main方法

---
+++ Ch1/test02.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//2.应用程序的入口必须是main方法：func main()
+func main1(){
+	fmt.Println("Hello Word!")
+}
+
+
+
+//运行结果如下：
+//λ go run 1.go
+//# command-line-arguments
+//runtime.main_main·f: function main is undeclared in the main package
