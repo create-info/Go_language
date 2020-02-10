2c21761fba7624a13cdedad1c2511814e1fc948d
just try it <614841384@qq.com>
2020-02-10 09:59:29 +0800

    Create test01.go

---
+++ Ch1/test01.go
@@ -0,0 +1,17 @@
+//应用程序的入口
+//1.必须是main包：package main
+package test
+
+import "fmt"
+
+
+func main(){
+	fmt.Println("Hello Word!")
+}
+
+
+//运行结果：
+//E:\goWorkPath
+//λ go run test01.go
+//go run: cannot run non-main package
+
